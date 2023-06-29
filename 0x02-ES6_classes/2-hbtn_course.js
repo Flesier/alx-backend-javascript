7f5e749bf0fe51f503f6d2db6e5c358d76ae3ae7
Flesier <flesieronyango18@gmail.com>
2023-06-29 16:41:12 +0300

    Implement a class named HolbertonCourse:
    Constructor attributes:
    name (String)
    length (Number)
    students (array of Strings)

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,49 @@
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+        if (typeof name !== 'string') {
+            throw new TypeError('Name must be a string');
+        } else {
+            this.name = name;
+        }
+        if (typeof length !== 'number') {
+            throw new TypeError('Length must be a number');
+        } else {
+            this.length = length;
+        }
+        if (!Array.isArray(students)) {
+            throw new TypeError('must be an array');
+        } else {
+            this._students = students;
+        }
+}
+
+get name() {
+    return this._name;
+}
+
+set name(na) {
+    if (typeof na !== 'string') {
+        throw new TypeError('Name must be a string');
+    }
+    this._name = na;
+}
+get length() {
+    return this.length;
+}
+set length(zise) {
+    if (typeof zise !== 'number') {
+        throw new TypeError('Length must be a number');
+    }
+    this.length = zise;
+}
+get students() {
+    return this._students;
+}
+
+set students(stud) {
+    if (!Array.isArray(stud)) {
+        throw new TypeError('must be an array');
+    }
+    this._students = stud;
+}
+}

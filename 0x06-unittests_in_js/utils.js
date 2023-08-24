b726d986863c92961faaeaf719465154dba958f8
Flesier <flesieronyango18@gmail.com>
2023-08-24 22:21:44 +0300

    Sinon is a library allowing you to create spies.

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,24 @@
+/**
+ * Contains utility functions for the application.
+ */
+ const Utils = {
+    calculateNumber: (type, a, b) => {
+      a = Math.round(a);
+      b = Math.round(b);
+      switch (type) {
+        case 'SUM':
+          return (a + b);
+        case 'SUBTRACT':
+          return (a - b);
+        case 'DIVIDE':
+          if (b === 0) {
+            return 'Error';
+          }
+          return (a / b);
+        default:
+          return 'Error';
+      }
+    }
+  };
+  
+  module.exports = Utils;
